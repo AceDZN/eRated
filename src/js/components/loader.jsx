b06482dc84fa09592eeeb42c270ca73e0be6fc37
AceDZN <design@acedzn.com>
2016-05-15 09:42:07 +0300

    + Added some dependencies - reflux & fetch + Added bootstrap css - maybe I'll use it later for the grid * Fixed loader component

--- src/js/components/loader.jsx
+++ src/js/components/loader.jsx
@@ -1,27 +1,15 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  getInitialState: function(){
-    return {
-      loaderLoadClass: ''
-    }
-  },
   getLoadClass: function(){
     if (this.props.loaded){
-      this.setState({
-        loaderLoadClass: 'loaded'
-      });
-    } else {
-      this.setState({
-        loaderLoadClass: ''
-      });
+      return  'loaded'
     }
+    return
   },
   render: function(){
       return (
-        <div className={"loader_wrap animate "+ this.state.loaderLoadClass}>
-
-        </div>
+        <div className={"loader_wrap animate "+ this.getLoadClass()}></div>
       );
   },
 
